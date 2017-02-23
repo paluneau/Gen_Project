ea4747a88d6d3afc49b36a716b3b871e1a92c597
DrunkenKiwwi <feck-to@hotmail.fr>
2017-02-23 14:57:17 -0500

    + Application FXML
    Link avec le projet et la vue

--- src/app/Application.java
+++ src/app/Application.java
@@ -1,11 +1,34 @@
 package app;
 
 import controleur.Controller;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
-public class Application {
-	
+public class Application extends Application {
+
 	private Controller ctrl = null;
-	private Scene scn = null;
+	
 
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("/vue/interface_v.01.fxml"));
+
+		BorderPane root = (BorderPane) loader.load();
+		ctrl = loader.getController();
+
+		Scene scene = new Scene(root);
+
+		stage.setTitle("Genomic Physionomy Viewer");
+		stage.setScene(scene);
+		stage.setResizable(false);
+		stage.show();
+
+	}
 }
