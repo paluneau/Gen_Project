4d1cc1ad5938403b7297016ba230f0c802f954a5
MisterDeenis <po.denis@hotmail.com>
2017-04-06 19:12:23 -0400

    + Continuité de l'algo pour les faces vides + Ajout du dernier OBJ de JC. + On peut trouver maintenant les points communs des différents groupes. (Il les sysout)

--- src/modele/phenotype/Points.java
+++ src/modele/phenotype/Points.java
@@ -1,8 +1,6 @@
 package modele.phenotype;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import javafx.collections.FXCollections;
@@ -12,11 +10,13 @@ public class Points {
 
 	private Map<String, ObservableFloatArray> points3DIni = null;
 	private Map<String, ObservableFloatArray> points3DUpdater = null;
+	private Map<String, ObservableFloatArray> pointsIniSupp = null;
 	private Map<String, ObservableFloatArray> pointsSupp = null;
 
 	public Points() {
 		points3DIni = new HashMap<String, ObservableFloatArray>();
 		points3DUpdater = new HashMap<String, ObservableFloatArray>();
+		pointsIniSupp = new HashMap<String, ObservableFloatArray>();
 		pointsSupp = new HashMap<String, ObservableFloatArray>();
 	}
 
@@ -32,36 +32,37 @@ public class Points {
 	public void updateDistanceOeilNez(float distance) {
 		updatePoints("Oeil gauche", -distance);
 		updatePoints("Oeil droit", distance);
+		// updatePoints("face1", distance);
+		// updatePoints("face2", distance);
 	}
 
-	public List<Float> findSiblings() {
-		/*
-		 * File fichierSave = new
-		 * File("C:/Users/1541254/Documents/GPV - ADN.txt"); PrintWriter printW
-		 * = null; try { printW = new PrintWriter(new FileWriter(fichierSave));
-		 * } catch (IOException e1) { e1.printStackTrace(); }
-		 */
-		for (ObservableFloatArray e : points3DIni.values()) {
-			for (ObservableFloatArray f : points3DIni.values()) {
+	public void findSiblings() {
+		for (int k = 0; k < points3DIni.values().size() - 1; k++) {
+			ObservableFloatArray e = (ObservableFloatArray) points3DIni.values().toArray()[k];
+			for (int l = k + 1; l < points3DIni.values().size(); l++) {
+				ObservableFloatArray f = (ObservableFloatArray) points3DIni.values().toArray()[l];
 				if (!e.equals(f)) {
 					for (int i = 0; i < e.size() / 3; i++) {
 						ObservableFloatArray p = FXCollections.observableFloatArray();
 						p.addAll(e.get(3 * i), e.get((3 * i) + 1), e.get((3 * i) + 2));
-						// System.out.println("P : " + p.toString());
 						for (int j = 0; j < f.size() / 3; j++) {
 							ObservableFloatArray q = FXCollections.observableFloatArray();
 							q.addAll(f.get(3 * j), f.get((3 * j) + 1), f.get((3 * j) + 2));
-							// System.out.println("Q : " + q.toString());
-
 							if (findIfEquals(p, q)) {
-								List<ObservableFloatArray> points = new ArrayList<ObservableFloatArray>();
-								points.add(q);
-								points.add(createArrayCopy(q));
-								pointsSupp.put(findKeyFromValueMap(e, points3DIni), q);
-								points = new ArrayList<ObservableFloatArray>();
-								points.add(p);
-								points.add(createArrayCopy(p));
-								pointsSupp.put(findKeyFromValueMap(f, points3DIni), p);
+								if (pointsSupp.containsKey(findKeyFromValueMap(e, points3DIni))) {
+									pointsIniSupp.get(findKeyFromValueMap(e, points3DIni)).addAll(createArrayCopy(q));
+									pointsSupp.get(findKeyFromValueMap(e, points3DIni)).addAll(q);
+								} else {
+									pointsIniSupp.put(findKeyFromValueMap(e, points3DIni), createArrayCopy(q));
+									pointsSupp.put(findKeyFromValueMap(e, points3DIni), q);
+								}
+								if (pointsSupp.containsKey(findKeyFromValueMap(f, points3DIni))) {
+									pointsIniSupp.get(findKeyFromValueMap(f, points3DIni)).addAll(createArrayCopy(p));
+									pointsSupp.get(findKeyFromValueMap(f, points3DIni)).addAll(p);
+								} else {
+									pointsIniSupp.put(findKeyFromValueMap(f, points3DIni), createArrayCopy(p));
+									pointsSupp.put(findKeyFromValueMap(f, points3DIni), p);
+								}
 							}
 						}
 
@@ -69,18 +70,15 @@ public class Points {
 				}
 
 			}
-			// printW.close();
+		}
 
-		}
-		for (ObservableFloatArray sdk : points3DIni.values()) {
-			System.out.println(findKeyFromValueMap(sdk, points3DIni));
-			for (int i = 0; i < sdk.size() / 3; i++) {
-				System.out.println(
-						"P : [" + sdk.get(3 * i) + ", " + sdk.get((3 * i) + 1) + ", " + sdk.get((3 * i) + 2) + "]");
-			}
-		}
-		System.out.println();
-		return null;
+		/*
+		 * for (ObservableFloatArray sdk : pointsSupp.values()) {
+		 * System.out.println(findKeyFromValueMap(sdk, pointsSupp)); for (int i
+		 * = 0; i < sdk.size() / 3; i++) { System.out.println( "P : [" +
+		 * sdk.get(3 * i) + ", " + sdk.get((3 * i) + 1) + ", " + sdk.get((3 * i)
+		 * + 2) + "]"); } } System.out.println();
+		 */
 	}
 
 	private boolean findIfEquals(ObservableFloatArray p, ObservableFloatArray q) {
@@ -119,10 +117,15 @@ public class Points {
 		for (int i = 0; i < points.size() / 3; i++) {
 			points.set(2 + (3 * i), points3DIni.get(group).get(2 + (3 * i)) + factor);
 		}
-		ObservableFloatArray pointSup = pointsSupp.get(group);
-		for (int i = 0; i < pointSup.size() / 3; i++) {
-			pointSup.set(2 + (3 * i), pointSup.get(2 + (3 * i)) + factor);
-		}
+
+		/*
+		 * if (group.contains("face2")) { ObservableFloatArray pointSup =
+		 * pointsSupp.get(group); for (int i = 0; i < pointSup.size() / 3; i++)
+		 * { pointSup.set(2 + (3 * i), pointsIniSupp.get(group).get(2 + (3 * i))
+		 * - factor); } System.out.println("SUPP : " + pointSup.get(2));
+		 * System.out.println("PP : " + points3DUpdater.get(group).get(2)); }
+		 */
+
 	}
 
 	private ObservableFloatArray createArrayCopy(ObservableFloatArray original) {
