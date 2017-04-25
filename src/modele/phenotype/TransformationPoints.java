c0b11cf8810dd06ee81bda2ff554e66c19e453e2
MisterDeenis <po.denis@hotmail.com>
2017-04-24 22:20:53 -0400

    + Ajout de méthodes utiles + Ajout d'une méthode permettant de déterminer la distance entre un point et un segment de droite formé à l'aide d'un autre point. + Ajout d'une méthode permettant de déterminer le point milieu d'un groupe de points. (Pour trouver le milieu du cou)

--- src/modele/phenotype/TransformationPoints.java
+++ src/modele/phenotype/TransformationPoints.java
@@ -57,7 +57,8 @@ public class TransformationPoints {
 		for (String group : part.getSubParts()) {
 			updatePointsTranslation(group, groupREM, transformation);
 		}
-		//updatePointsTranslation(part.getSubParts().get(0), groupREM, transformation);
+		// updatePointsTranslation(part.getSubParts().get(0), groupREM,
+		// transformation);
 
 	}
 
@@ -178,20 +179,90 @@ public class TransformationPoints {
 		}
 	}
 
-	private ObservableFloatArray removeValue(ObservableFloatArray points, List<Integer> toBeRemoved) {
-		ObservableFloatArray out = FXCollections.observableFloatArray();
-		float[] tempPoints = points.toArray(null);
-		System.out.println(toBeRemoved.toString());
-		for (int i = 0; i < tempPoints.length / 3; i++) {
-			if (!toBeRemoved.contains(new Integer(i))) {
-				out.addAll(tempPoints[i]);
-				out.addAll(tempPoints[i + 1]);
-				out.addAll(tempPoints[i + 2]);
-			}
-		}
+	/**
+	 * Trouve la distance entre le segment de droite PR et le point Q.
+	 * 
+	 * @param p
+	 *            - point 1 sur la droite
+	 * @param r
+	 *            - point 2 sur la droite
+	 * @param q
+	 *            - point que l'on veut savoir la distance
+	 * @return la distance entre le point Q et la droite PR
+	 */
+	private double findDistance(Point3D p, Point3D r, Point3D q) {
+		Point3D pq = findVecteur(p, q);
+		Point3D d = findVecteur(p, r);
+		return findNorme(produitVectoriel(pq, d)) / findNorme(d);
+	}
 
-		// System.out.println(newbie.toString());
-		return out;
+	/**
+	 * Calcule le produit vectoriel de deux vecteurs
+	 * 
+	 * @param u
+	 *            - vecteur 1
+	 * @param v
+	 *            - vecteur 2
+	 * @return le vecteur du produit vectoriel
+	 */
+	private Point3D produitVectoriel(Point3D u, Point3D v) {
+		double x = (u.getY() * v.getZ()) - (v.getY() * u.getZ());
+		double y = -((u.getX() * v.getZ()) - (v.getX() * u.getZ()));
+		double z = (u.getX() * v.getY()) - (v.getX() * u.getY());
+		return new Point3D(x, y, z);
+	}
+
+	/**
+	 * Trouve le vecteur à l'aide de deux points
+	 * 
+	 * @param p
+	 *            - point 1
+	 * @param q
+	 *            - point 2
+	 * @return le vecteur (directeur)
+	 */
+	private Point3D findVecteur(Point3D p, Point3D q) {
+		double x = q.getX() - p.getX();
+		double y = q.getY() - p.getY();
+		double z = q.getZ() - p.getZ();
+		return new Point3D(x, y, z);
+	}
+
+	/**
+	 * Trouve la norme du vecteur
+	 * 
+	 * @param p
+	 *            - vecteur
+	 * @return la norme en double
+	 */
+	private double findNorme(Point3D p) {
+		double x = Math.pow(p.getX(), 2);
+		double y = Math.pow(p.getY(), 2);
+		double z = Math.pow(p.getZ(), 2);
+		return Math.sqrt(x + y + z);
+	}
+
+	/**
+	 * Méthode permettant de trouver le point milieu d'un groupe (utile pour
+	 * déplacer le cou) TODO tests la dessus svp
+	 * 
+	 * @param pointsGroup
+	 *            - les points du groupe en question
+	 * @return le point milieu du groupe
+	 */
+	private Point3D findPointMilieu(ObservableFloatArray pointsGroup) {
+		double moyX = 0;
+		double moyY = 0;
+		double moyZ = 0;
+		for (int i = 0; i < pointsGroup.size() / 3; i++) {
+			moyX += pointsGroup.get(3 * i);
+			moyY += pointsGroup.get((3 * i) + 1);
+			moyZ += pointsGroup.get((3 * i) + 2);
+		}
+		moyX = moyX / (pointsGroup.size() / 3);
+		moyY = moyY / (pointsGroup.size() / 3);
+		moyZ = moyZ / (pointsGroup.size() / 3);
+		return new Point3D(moyX, moyY, moyZ);
 	}
 
 	/**
